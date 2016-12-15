5006b6686d35df5b9c97c92e446371f96ceea432
erjosito <erjosito@hotmail.com>
2016-12-15 11:42:09 +0100

    resgrp variable

--- create_vm.sh
+++ create_vm.sh
@@ -1,4 +1,4 @@
-ansible-playbook ./azure_playbooks/new_vm_web.yml --extra-vars "vmname=$1"
+ansible-playbook ./azure_playbooks/new_vm_web.yml --extra-vars "vmname=$1 rscgrp=$2"
 # At this point the VM is new, we need to skip the known_hosts check
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i ./azure_rm.py ./azure_playbooks/httpd.yml --extra-vars "vmname=$1"
